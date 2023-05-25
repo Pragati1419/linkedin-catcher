33448eee78e148bbf06a791d52868cbb1a22c27d
Pragati Kumari <92241659+Pragati1419@users.noreply.github.com>
2023-05-25 15:49:34 +0530

    Update test.js
    Testing the functionality

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const scrapper = require("../src/index")
 
 scrapper({
-    url:"https://www.linkedin.com/in/pragati1157/", // ex: Pragati Kumari/
+    url:"https://www.linkedin.com/in/pragati1157/",  // ex: Pragati Kumari/
 }).then(res=>console.warn(res));
 
 
